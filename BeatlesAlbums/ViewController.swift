f4604490dc7965489b92a74ee87fcb8e1289da54
Jon Compton <comptonjon1@gmail.com>
2020-04-18 09:33:58 -0700

    Initial Commit

---
+++ BeatlesAlbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeatlesAlbums
+//
+//  Created by Jonathan Compton on 4/18/20.
+//  Copyright © 2020 Jonathan Compton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
